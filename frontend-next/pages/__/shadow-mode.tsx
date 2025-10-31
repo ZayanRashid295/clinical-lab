9fbba4d64fbd43e42381cd4dff4d94f13bb99d06
ahmadhassanch <ahmadhassanch@hotmail.com>
2025-10-31 15:58:43 +0500

    Remove ShadowModePage component to streamline the codebase and eliminate unused features.

--- frontend-next/pages/__/shadow-mode.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ShadowModeContent from "../../src/app/components/ShadowMode/ShadowModeContent";
-
-const ShadowModePage1: React.FC = () => {
-  return <ShadowModeContent />;
-};
-
-export default ShadowModePage1;
